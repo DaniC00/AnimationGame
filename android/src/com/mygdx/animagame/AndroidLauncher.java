494063932ee8ce6222e2845d9ea650b570a010e2
Dani <dcalper00@gmail.com>
2021-06-06 22:43:10 +0200

    initial commit

---
+++ android/src/com/mygdx/animagame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.animagame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.animagame.AnimaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AnimaGame(), config);
+	}
+}
